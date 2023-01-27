1fe7729e1850911b49989c28fbd37bb57d88d15e
Taivnaa032 <etaivnaa032@gmail.com>
2023-01-27 17:22:25 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -3,30 +3,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const linkRouter = require("./router/linkRouter");
 const userRouter = require("./router/userRouter");
-const app = express();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const connect = require("./database");
 require("dotenv").config();
 
-const port = process.env.PORT ;
+const port = process.env.PORT;
 
-
-mongoose.connect(
-  process.env.MONGODB_URL, 
-  { useNewUrlParser: true }
-);
-
-mongoose.connection.once("open", () => {
-  console.log("Connected to Mongo");
-});
-
+const app = express();
+connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use("/link", linkRouter); 
+app.use("/link", linkRouter);
 app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-
-
